533d2e269cb0c0bb4574312acb2233cede481320
linghel marla <linghel.marlacms@gmail.com>
2022-02-22 16:58:40 -0300

    Entendendo mais sobre sentença e comentarios

---
+++ inicio/sentença_2.js
@@ -0,0 +1,10 @@
+console.log("ola!")
+
+//apreendendo sentença de codigo, no caso a baixo tenho 3 sentenças de codigo!
+
+console.log("Boa tarde!")
+console.log("Bom Dia!")
+console.log("Boa Noite!")
+
+/* Apos essa aula o professor criou uma aula apenas para explicar sobre os comentarios , como fazer 
+e etc , optei por nao criar uma pasta para esse assunto*/
