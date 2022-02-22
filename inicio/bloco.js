47481e52a990d020f030b36a990436e11279eb87
linghel marla <linghel.marlacms@gmail.com>
2022-02-22 17:33:50 -0300

    Desafio_1

---
+++ inicio/bloco.js
@@ -0,0 +1,16 @@
+//Bloco é definido atraves de um par de chaves
+{
+    console.log("Passo #1") //aqui temos uma sentença dentro de um bloco , podemos agrupar varias sentenças dentro de um bloco
+}
+
+//veja o exemplo de 5 sentença de cogigo e 2 blocos :
+{
+    console.log("passo#1"); 
+    console.log("passo#2");
+    console.log("passo#3");
+}
+
+{
+    console.log("passo#4"); 
+    console.log("passo#5");
+}
---
+++ inicio/desafio1.js
@@ -0,0 +1,19 @@
+// exiba no console numeros impares e pares de forma separada
+
+console.log("Os Numeros impares Sao:");
+{
+    console.log("Primeiro 1");
+    console.log("segundoo 3");
+    console.log("terceiro 5");
+    console.log("Quarto 7");
+    console.log("quinto 9");
+}
+console.log("Os Numeros pares Sao:");
+{
+    console.log("Primeiro 2");
+    console.log("segundoo 4");
+    console.log("terceiro 6");
+    console.log("Quarto 8");
+    console.log("quinto 10");
+
+}
