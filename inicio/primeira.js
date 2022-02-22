85c1d4eabe0dc7908a8308166ececac7befa9804
linghel marla <linghel.marlacms@gmail.com>
2022-02-22 16:17:07 -0300

    Aprendendo sentença de codigo

---
+++ inicio/primeira.js
@@ -0,0 +1,7 @@
+console.log("ola!")
+
+//apreendendo sentença de codigo, no caso a baixo tenho 3 sentenças de codigo//
+
+console.log("Boa tarde!")
+console.log("Bom Dia!")
+console.log("Boa Noite!")
